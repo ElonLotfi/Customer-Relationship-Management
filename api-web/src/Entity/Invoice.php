6e8b241df1ea4b8ee46444de4e96dde9b4e3930e
JugheadTn <med.stark17@gmail.com>
2020-03-23 23:36:57 +0100

    pagination api

--- api-web/src/Entity/Invoice.php
+++ api-web/src/Entity/Invoice.php
@@ -8,7 +8,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\InvoiceRepository")
- * @ApiResource
+ * @ApiResource(attributes={"pagination_enabled"=true})
  */
 class Invoice
 {
