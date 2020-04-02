1f62b48dd0b88ce3030fc954f8f8eee42db5c165
JugheadTn <med.stark17@gmail.com>
2020-04-03 00:53:57 +0200

    recuperation des customers depuis la base de donnés
    + recuperation de customers :+1:
    utilisation de useState , useEffect et axios

--- api-web/assets/js/pages/Clients.jsx
+++ api-web/assets/js/pages/Clients.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Client = () => {
+//table.table-hover pour creer le tableau
+//thead>tr>th*7 pour creer l'entete de tableau
+//tbody>tr>td*7 pour creer le body de tableau
+
+const Client = props => {
+  const [customer, setCustomer] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://127.0.0.1:8000/api/customers")
+      .then(response => response.data["hydra:member"])
+      .then(data => setCustomer(data))
+      .catch(error => console.log(error.response));
+  }, []);
+
   return (
     <>
       <h1>List des Clients</h1>
+
       <table className="table table-hover">
         <thead>
           <tr>
@@ -18,21 +33,27 @@ const Client = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>19</td>
-            <td>
-              <a>Lotfi mhamed</a>
-            </td>
-            <td>lotfi@med.fr</td>
-            <td>lotfi Inc</td>
-            <td className="text-center">
-              <span className="badge badge-dark">3</span>
-            </td>
-            <td className="text-center">2 366.00 E</td>
-            <td>
-              <button className="btn btn-sm btn-danger">Supprimer</button>
-            </td>
-          </tr>
+          {customer.map(customers => (
+            <tr key={customers.id}>
+              <td>{customers.id}</td>
+              <td>
+                <a>
+                  {customers.firstname} {customers.lastname}
+                </a>
+              </td>
+              <td>{customers.email}</td>
+              <td>{customers.company}</td>
+              <td className="text-center">
+                <span className="badge badge-dark">3</span>
+              </td>
+              <td className="text-center">
+                {customers.totalAmount.toLocaleString()}
+              </td>
+              <td>
+                <button className="btn btn-sm btn-danger">Supprimer</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
