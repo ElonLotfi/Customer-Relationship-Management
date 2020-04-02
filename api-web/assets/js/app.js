f258c60a4944a596fa75e8f52175b3537afa21f9
JugheadTn <med.stark17@gmail.com>
2020-04-02 23:39:14 +0200

    installation de React.Js
    add React Js

--- api-web/assets/js/app.js
+++ api-web/assets/js/app.js
@@ -1,14 +1,26 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import "../css/app.css";
+import Home from "./pages/Home";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Clients from "./pages/Clients";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+const App = () => {
+  return (
+    <HashRouter>
+      <Navbar />
 
-console.log('salut lotfi');
+      <main className="container pt-5">
+        <Switch>
+          <Route path="/customers" component={Clients}></Route>
+          <Route path="/" component={Home} />
+        </Switch>
+      </main>
+    </HashRouter>
+  );
+};
+const rootElement = document.querySelector("#app");
+ReactDOM.render(<App />, rootElement);
