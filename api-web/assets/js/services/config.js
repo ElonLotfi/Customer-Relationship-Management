e2e27e6bd926cdafd8bd4b5a89cc96e47c463c63
JugheadTn <med.stark17@gmail.com>
2020-04-19 01:24:05 +0200

    refactorisation du code

--- api-web/assets/js/services/config.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = process.env.API_URL;
-
-export const API_CUSTOMER = API_URL + "customers";
-export const API_INVOICE = API_URL + "invoices";
-export const API_USERS = API_URL + "users";
-export const API_LOGIN = API_URL + "login_check";
