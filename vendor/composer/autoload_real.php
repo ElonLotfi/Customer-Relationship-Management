01c65d13dea6b1bb4793c304cf75200f09345c95
JugheadTn <med.stark17@gmail.com>
2020-04-20 01:02:18 +0200

    update api

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit9b6c082b73b90da67aec4b4411893f0e
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
