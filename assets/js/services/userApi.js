7321b0207ca9c01c089579dee3270c1791f35153
JugheadTn <med.stark17@gmail.com>
2020-04-19 02:33:05 +0200

    modification de service axios

--- assets/js/services/userApi.js
+++ assets/js/services/userApi.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import { API_USERS } from "./config";
 function addUser(user) {
-  Axios.post("https://mhamed-app.herokuapp.com/api/", user).then(
+  Axios.post("https://mhamed-app.herokuapp.com/api/users", user).then(
     (response) => response.data
   );
 }
