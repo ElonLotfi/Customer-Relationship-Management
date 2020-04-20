04238d92c8c58f3686c8bab89ccbcae2b62b1351
JugheadTn <med.stark17@gmail.com>
2020-04-20 03:27:08 +0200

    Update userApi.js

--- assets/js/services/userApi.js
+++ assets/js/services/userApi.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import { API_USERS } from "../config";
 function addUser(user) {
-  Axios.post(API_USERS, user).then((response) => response.data);
+  return Axios.post(API_USERS, user).then((response) => response.data);
 }
 
 export default {
